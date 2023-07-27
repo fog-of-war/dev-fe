83bf21309e8b2e5c4e132eb76070a62b50327306
jang_yoonsu <shagrat8546@gmail.com>
2023-07-27 21:42:54 +0900

    Feat : 이모션 테마 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import Map from "./GoogleMap";
+import { css } from "@emotion/css";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./constants/theme";
+
+import CertificationModal from "./components/CertificationModal";
 
 function App() {
   return (
-    <div>
-      <Map />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div
+        className={css`
+          width: 100%;
+          height: 100vh;
+          background-color: grey;
+        `}
+      >
+        Hellow World!
+        <CertificationModal />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/constants/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    primary: "#53AF7B",
+    secondary: "#11522D",
+    tertiary: "#E4F6ED",
+    mainFont: "#414141",
+    subFont: "6F6F6F",
+  },
+};
