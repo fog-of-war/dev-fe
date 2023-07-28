7367df47c093de50b401c5c61c1fcbdce71fcf3b
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-07-28 23:35:05 +0900

    Refactor : 검은색 폴리곤과 서울 구를 덮는 폴리곤 컴포넌트화

--- src/components/Map/GoogleMap.tsx
+++ src/components/Map/GoogleMap.tsx
@@ -1,31 +1,17 @@
 import { useState, useEffect, useRef } from "react";
-import { GoogleMap, LoadScriptNext, Polygon } from "@react-google-maps/api";
+import { GoogleMap, LoadScriptNext } from "@react-google-maps/api";
 
 import seoulData from "../../data/seoul2.json";
 import retroMapStyle from "../../data/retroMapStyle.json";
-import {
-  defaultCenter,
-  OUTER_BOUNDS,
-  bounds,
-  options,
-  COORDINATES_STRING,
-} from "../../data/mapData";
+import { defaultCenter, bounds, options } from "../../data/mapData";
+
+import SeoulPolygon from "./SeoulPolygon";
+import BlackPolygon from "./BlackPolygon";
 
 const containerStyle = {
-  width: "650px",
-  height: "600px",
+  width: "375px",
+  height: "667px",
 };
-
-const parseCoordinates = (coordinateString: string) => {
-  const coordinatePairs = coordinateString.trim().split(" ");
-  return coordinatePairs.map((pair) => {
-    const [lng, lat] = pair.split(",");
-    return { lat: parseFloat(lat), lng: parseFloat(lng) };
-  });
-};
-
-// 서울 지역의 좌표를 파싱하고 순서를 반전
-const seoulCoordinates = parseCoordinates(COORDINATES_STRING);
 
 const Map = () => {
   const [polygons, setPolygons] = useState<
@@ -106,7 +92,7 @@ const Map = () => {
               latLngBounds: bounds,
               strictBounds: false,
             },
-            styles: retroMapStyle, // retro맵 스타일 적용
+            styles: retroMapStyle,
           }}
           onLoad={(map) => {
             mapRef.current = map;
@@ -116,45 +102,20 @@ const Map = () => {
             setZoomLevel(newZoomLevel);
           }}
         >
-          {/* 한반도를 덮는 검은색 폴리곤 */}
-          <Polygon
-            paths={[OUTER_BOUNDS, seoulCoordinates]}
-            options={{
-              fillColor: "#000",
-              fillOpacity: 1,
-              strokeOpacity: 0,
-            }}
-          />
-          {/* 서울 지역을 표시하는 다른 색상의 폴리곤 */}
-          {polygons.map((polygon, index) => {
-            const name_eng = polygon.name_eng;
-            const point = options[name_eng].point;
-            let opacity =
-              point < 1000
-                ? 0.9
-                : point < 2000
-                ? 0.7
-                : point < 3000
-                ? 0.5
-                : 0.3;
-            return (
-              <Polygon
-                key={index}
-                paths={polygon.path}
-                options={{
-                  fillColor: "#222",
-                  fillOpacity: opacity,
-                  strokeColor: "#555",
-                  strokeOpacity: 1,
-                  strokeWeight: 4,
-                }}
-                onClick={() => {
-                  const center = getCentroid(polygon.path);
-                  setView({ center, zoom: 16 }); // 먼저 줌 레벨을 변경하고 그 다음에 중심을 변경
-                }}
-              />
-            );
-          })}
+          {/* BlackPolygon 컴포넌트와 SeoulPolygon 컴포넌트 */}
+          <BlackPolygon />
+          {polygons.map((polygon, index) => (
+            <SeoulPolygon
+              key={index}
+              nameEng={polygon.name_eng}
+              path={polygon.path}
+              point={options[polygon.name_eng].point}
+              onPolygonClick={() => {
+                const center = getCentroid(polygon.path);
+                setView({ center, zoom: 16 });
+              }}
+            />
+          ))}
         </GoogleMap>
       </LoadScriptNext>
     </div>
