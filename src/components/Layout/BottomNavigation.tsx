5be483d7ff5e2a0b035195fd48fffcc886da8a70
jang_yoonsu <shagrat8546@gmail.com>
2023-07-31 23:49:48 +0900

    Feat : 바텀 네비게이션 구현

---
+++ src/components/Layout/BottomNavigation.tsx
@@ -0,0 +1,56 @@
+/** @jsxImportSource @emotion/react */
+
+import { useNavigate } from "react-router-dom";
+import BottomNavIcon from "./BottomNavButton";
+
+const BottomNavigation = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      css={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+        height: 80,
+        backgroundColor: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+        padding: "0 10px",
+        zIndex: "50",
+      }}
+    >
+      <BottomNavIcon
+        onClick={() => navigate("/")}
+        src="/images/homeIcon.png"
+        alt="home"
+      />
+      <BottomNavIcon
+        onClick={() => navigate("/explore")}
+        src="/images/mapIcon.png"
+        alt="map"
+      />
+      <BottomNavIcon
+        onClick={() => navigate("/camera")}
+        height={48}
+        src="/images/cameraIcon.png"
+        alt="home"
+      />
+
+      <BottomNavIcon
+        onClick={() => navigate("/notifications")}
+        src="/images/alarmIcon.png"
+        alt="home"
+      />
+      <BottomNavIcon
+        onClick={() => navigate("/profile")}
+        src="/images/profileIcon.png"
+        alt="home"
+      />
+    </div>
+  );
+};
+
+export default BottomNavigation;
