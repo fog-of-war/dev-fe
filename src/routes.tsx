4b423f1b2700ea4ff8676558ac834812eb545449
H0onnn <dukei2017@naver.com>
2023-08-02 21:39:32 +0900

    Feat : MyPage 라우터 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import MyPage from "./pages/MyPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" Component={Home} />
+      <Route path="/profile" Component={MyPage} />
     </Routes>
   );
 };
