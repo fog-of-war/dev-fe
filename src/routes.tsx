12e8bbd974878df11b1d1b859a694d4e45f63675
jang_yoonsu <shagrat8546@gmail.com>
2023-08-03 15:49:32 +0900

    Feat : auth페이지 구현

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import AuthPage from "./pages/Auth";
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/auth" element={<AuthPage />} />
       <Route path="/" Component={Home} />
     </Routes>
   );
