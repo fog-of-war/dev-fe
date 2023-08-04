65d18c7fd0e6b4f8c52d955411a1f86d346e4388
H0onnn <dukei2017@naver.com>
2023-08-04 14:11:59 +0900

    Feat : 리뷰 페이지 라우터 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import AuthPage from "./pages/Auth";
 import MyPage from "./pages/MyPage";
 import Map from "./components/Map/GoogleMap";
-import ReviewList from "./pages/ReviewList";
+import ReviewPage from "./pages/ReviewPage";
 
 const AppRoutes = () => {
   return (
@@ -12,7 +12,7 @@ const AppRoutes = () => {
       <Route path="/" Component={Home} />
       <Route path="/profile" Component={MyPage} />
       <Route path="/map" Component={Map} />
-      <Route path="/reviewList" Component={ReviewList} />
+      <Route path="/reviewList" Component={ReviewPage} />
     </Routes>
   );
 };
