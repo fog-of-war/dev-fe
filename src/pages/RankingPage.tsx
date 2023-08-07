838c55747fcd86d1ef5d6c27c7ff3badb7437cea
H0onnn <dukei2017@naver.com>
2023-08-07 23:12:33 +0900

    Feat : 탐험 랭킹 페이지 구현

--- src/pages/RankingPage.tsx
+++ src/pages/RankingPage.tsx
@@ -3,6 +3,39 @@
 import ProfileCard from "../components/ProfileCard/ProfileCard";
 import ProfileInfo from "../components/ProfileCard/ProfileInfo";
 import ProgressBar from "../components/ProgressBar";
+import MyRanking from "../components/MyRanking";
+import TotalRanking from "../components/Ranking/TotalRanking";
+import Title from "../components/Title";
+
+const DUMMY_DATA = [
+  {
+    ranking: 1,
+    profileImage: "https://source.unsplash.com/random",
+    nickname: "예쁘게죽어가는김정훈",
+    flagIcon: "/images/flagIcon.png",
+    badgeIcon: "/images/badgeIcon.svg",
+    advanturePoint: 15000,
+    badgeCount: 3,
+  },
+  {
+    ranking: 2,
+    profileImage: "https://source.unsplash.com/random",
+    nickname: "살아나는뽀윤득수가좌",
+    flagIcon: "/images/flagIcon.png",
+    badgeIcon: "/images/badgeIcon.svg",
+    advanturePoint: 15000,
+    badgeCount: 3,
+  },
+  {
+    ranking: 3,
+    profileImage: "https://source.unsplash.com/random",
+    nickname: "죽어가는최애의동균님",
+    flagIcon: "/images/flagIcon.png",
+    badgeIcon: "/images/badgeIcon.svg",
+    advanturePoint: 15000,
+    badgeCount: 3,
+  },
+];
 
 const RankingPage = () => {
   return (
@@ -16,6 +49,32 @@ const RankingPage = () => {
       <ProfileCard />
       <ProfileInfo />
       <ProgressBar progress={80} />
+      <div
+        css={{
+          marginTop: "10px",
+        }}
+      >
+        <MyRanking />
+      </div>
+      <Title text="전체 랭킹 확인하기" icon="./images/mapIcon.png" />
+      <div
+        css={{
+          borderTop: "1px solid #e1e1e1",
+        }}
+      >
+        {DUMMY_DATA.map((data, index) => (
+          <TotalRanking
+            key={index}
+            ranking={data.ranking}
+            profileImage={data.profileImage}
+            nickname={data.nickname}
+            flagIcon={data.flagIcon}
+            badgeIcon={data.badgeIcon}
+            advanturePoint={data.advanturePoint}
+            badgeCount={data.badgeCount}
+          />
+        ))}
+      </div>
     </div>
   );
 };
