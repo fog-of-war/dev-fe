96979bdf5b0144ad20c17ade6536139bf1fc6e49
H0onnn <dukei2017@naver.com>
2023-08-07 21:02:04 +0900

    Design : css 수정

--- src/components/Review/ReviewAuthor.tsx
+++ src/components/Review/ReviewAuthor.tsx
@@ -2,7 +2,6 @@
 
 import colors from "../../constants/colors";
 import { AuthorInfo } from "../../types/types";
-
 interface ReviewAuthorProps {
   authorInfo: AuthorInfo;
 }
@@ -13,44 +12,50 @@ const ReviewAuthor = ({ authorInfo }: ReviewAuthorProps) => {
       css={{
         width: "100%",
         display: "flex",
-        flexDirection: "row",
-        padding: "0 20px",
-        gap: "10px",
         alignItems: "center",
       }}
     >
       <div
         css={{
-          width: "40px",
-          minWidth: "40px",
-          heigth: "40px",
-          minHeight: "40px",
-          overflow: "hidden",
-          borderRadius: "100%",
           display: "flex",
-          alignItems: "end",
+          alignItems: "center",
+          width: "100%",
+          gap: "10px",
         }}
       >
-        <img
+        <div
           css={{
-            objectFit: "cover",
-            width: "100%",
-            height: "100%",
-          }}
-          src={authorInfo.profileImage}
-          alt="profileImage"
-        />
-      </div>
-      <div>
-        <p
-          css={{
-            fontSize: "16px",
-            fontWeight: "600",
-            color: colors.primary,
+            width: "40px",
+            minWidth: "40px",
+            heigth: "40px",
+            minHeight: "40px",
+            overflow: "hidden",
+            borderRadius: "100%",
+            display: "flex",
+            alignItems: "end",
           }}
         >
-          {authorInfo.nickname}
-        </p>
+          <img
+            css={{
+              objectFit: "cover",
+              width: "100%",
+              height: "100%",
+            }}
+            src={authorInfo.profileImage}
+            alt="profileImage"
+          />
+        </div>
+        <div>
+          <p
+            css={{
+              fontSize: "16px",
+              fontWeight: "600",
+              color: colors.primary,
+            }}
+          >
+            {authorInfo.nickname}
+          </p>
+        </div>
       </div>
     </div>
   );
