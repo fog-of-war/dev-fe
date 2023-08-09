577915e89271a5c775f43077f5553aaae969b69c
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-08-09 18:45:52 +0900

    Add : 맵페이지 라우팅

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,7 @@ import MyPage from "./pages/MyPage";
 import AuthPage from "./pages/auth/AuthPage";
 import ProfileSetupPage from "./pages/auth/ProfileSetupPage";
 import ReviewPage from "./pages/ReviewPage";
-// import MapPage from "./pages/MapPage";
+import MapPage from "./pages/MapPage";
 import RankingPage from "./pages/RankingPage";
 import ProfileEditPage from "./pages/ProfileEditPage";
 import BadgeListPage from "./pages/BadgeListPage";
@@ -17,7 +17,7 @@ const AppRoutes = () => {
       <Route path="/" Component={Home} />
       <Route path="/profile" Component={MyPage} />
       <Route path="/profile_edit" Component={ProfileEditPage} />
-      {/* <Route path="/map" Component={MapPage} /> */}
+      <Route path="/map" Component={MapPage} />
       <Route path="/reviewList" Component={ReviewPage} />
       <Route path="/ranking" Component={RankingPage} />
       <Route path="/badgeList" Component={BadgeListPage} />
