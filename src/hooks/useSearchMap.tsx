d2585c2938ee27e3719ee4b6b1c9af9dfadde6f9
jang_yoonsu <shagrat8546@gmail.com>
2023-08-10 00:21:12 +0900

    Feat : 지도검색 커스텀훅에 최신검색어 저장 로직 추가

--- src/hooks/useSearchMap.tsx
+++ src/hooks/useSearchMap.tsx
@@ -1,11 +1,17 @@
 /** @jsxImportSource @emotion/react */
 
-import { KeyboardEvent, SetStateAction, useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import { debounce } from "lodash";
+import { useSetRecoilState } from "recoil";
+import { searchState } from "../store/searchAtom";
+import { Search } from "../types/types";
+import { useNavigate } from "react-router-dom";
 
 const useSearchMap = () => {
   const [inputValue, setInputValue] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
+  const setRecentSearches = useSetRecoilState(searchState);
+  const navigate = useNavigate();
 
   // 실시간 인풋 디바운싱하여 서치쿼리로 넘기는 함수
   const debounceInput = debounce(
@@ -22,12 +28,32 @@ const useSearchMap = () => {
 
   // 리액트 쿼리
 
-  console.log("검색 :" + inputValue);
-  console.log("디바운스 검색:" + searchQuery);
+  const updateRecentSearches = (newRecentSearch: Search) => {
+    setRecentSearches((prevSearches: Search[]) => {
+      // 최신 검색어를 배열 맨 앞에 추가하고 최대 개수를 유지합니다.
+      const updatedSearches = [
+        newRecentSearch,
+        ...prevSearches.filter(
+          (search: Search) => search.search !== newRecentSearch.search
+        ),
+      ];
+      return updatedSearches.slice(0, 10);
+    });
+  };
 
-  const handleSearchMap = (event: KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
+  const handleSearchMap = (
+    e: React.KeyboardEvent<HTMLInputElement> & { isComposing: boolean }
+  ) => {
+    if (e.isComposing || e.keyCode === 229) return;
+    if (e.key === "Enter") {
       console.log("검색어:", inputValue);
+      const newRecentSearch = {
+        id: Date.now(),
+        search: inputValue,
+        type: "keword",
+      };
+      updateRecentSearches(newRecentSearch);
+      navigate(`/search?query=${inputValue}`);
       // 검색 로직
     }
   };
