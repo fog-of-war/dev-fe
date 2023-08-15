14382d11abc6fabd1baa00e73a62f9bb136e8c0d
jang_yoonsu <shagrat8546@gmail.com>
2023-08-15 20:03:57 +0900

    Feat : 이미지 리스트 더보기 버튼 추가

--- src/components/Place/PlaceImageList.tsx
+++ src/components/Place/PlaceImageList.tsx
@@ -1,11 +1,16 @@
 /** @jsxImportSource @emotion/react */
 
+import { useNavigate } from "react-router-dom";
+import B2 from "../UI/B2";
+
 interface PlaceImageListProps {
   images: string[];
   displayAmount: 3 | 4;
 }
 
 const PlaceImageList = ({ images, displayAmount }: PlaceImageListProps) => {
+  const navigate = useNavigate();
+
   return (
     <div
       css={{
@@ -14,10 +19,11 @@ const PlaceImageList = ({ images, displayAmount }: PlaceImageListProps) => {
         gap: "10px",
       }}
     >
-      {images.slice(0, displayAmount).map((image) => (
+      {images.slice(0, displayAmount).map((image, i) => (
         <div
           key={image}
           css={{
+            position: "relative",
             flexGrow: 1,
             aspectRatio: displayAmount === 3 ? "4/3" : "1/1",
             borderRadius: "10px",
@@ -30,6 +36,37 @@ const PlaceImageList = ({ images, displayAmount }: PlaceImageListProps) => {
             alt="review_image"
             css={{ width: "100%", objectFit: "cover" }}
           />
+          {i === displayAmount - 1 && (
+            <div
+              onClick={() => navigate("/place/1")}
+              css={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                display: "flex",
+                alignItems: "center",
+                gap: "5px",
+              }}
+            >
+              <img
+                src="/images/place/cameraIcon.png"
+                alt="camera"
+                height={15}
+                css={{
+                  boxShadow: "1px 1px 1px rgba(0, 0, 0, 0.50)",
+                }}
+              />
+              <B2
+                css={{
+                  color: "#fff",
+                  textShadow: "1px 1px 1px rgba(0, 0, 0, 0.50)",
+                }}
+              >
+                {images.length}
+              </B2>
+            </div>
+          )}
         </div>
       ))}
     </div>
