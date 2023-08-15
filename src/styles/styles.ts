27cde02354dbdcc09cb0395ce68a37b4e12fa75c
H0onnn <dukei2017@naver.com>
2023-08-15 18:16:48 +0900

    Design : 뱃지 획득 페이지 레이아웃 추가

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -22,3 +22,15 @@ export const SearchBarLayout = styled.div`
   background: white;
   z-index: 70;
 `;
+
+export const GetBadgePageLayout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background-color: white;
+  gap: 3px;
+`;
