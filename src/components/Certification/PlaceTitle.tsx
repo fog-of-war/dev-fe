b198d0a55c9de3c945e36e5fb05b39a5ac3be3ed
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-08-15 16:55:45 +0900

    Desing : 카테고리 박스 디자인

--- src/components/Certification/PlaceTitle.tsx
+++ src/components/Certification/PlaceTitle.tsx
@@ -11,6 +11,38 @@ const PlaceTitle = ({ icon, name, category }: PlaceTitleProps) => {
   // 글자 수가 16자 이상인 경우에는 줄이고 '...'을 붙임
   const truncatedName = name.length > 16 ? name.slice(0, 16) + "..." : name;
 
+  // 카테고리에 따라 아이콘을 다르게 설정
+  let categoryIcon;
+
+  // 변환된 카테고리 이름을 저장할 변수 추가
+  let displayCategory;
+
+  switch (category) {
+    case "역사":
+      categoryIcon = "/images/certificationModal/historyIcon.png";
+      displayCategory = "역사";
+      break;
+    case "맛집":
+      categoryIcon = "/images/certificationModal/restaurantIcon.png";
+      displayCategory = "미식";
+      break;
+    case "스포츠시설":
+      categoryIcon = "/images/certificationModal/healthIcon.png";
+      displayCategory = "운동";
+      break;
+    case "미술관":
+      categoryIcon = "/images/certificationModal/artIcon.png";
+      displayCategory = "미술관";
+      break;
+    case "커피":
+      categoryIcon = "/images/certificationModal/coffeeIcon.png";
+      displayCategory = "커피";
+      break;
+    default:
+      categoryIcon = ""; // 디폴트 아이콘 경로 설정
+      displayCategory = category; // 변환 없음
+  }
+
   return (
     <div
       css={{
@@ -21,7 +53,6 @@ const PlaceTitle = ({ icon, name, category }: PlaceTitleProps) => {
       }}
     >
       <div css={{ display: "flex", alignItems: "center", gap: 5 }}>
-        {/* <img src={icon} alt="categoryIcon" width={16} height={20} /> */}
         <h1
           css={{
             fontSize: "20px",
@@ -44,9 +75,11 @@ const PlaceTitle = ({ icon, name, category }: PlaceTitleProps) => {
           fontSize: "14px",
           fontWeight: 600,
           boxShadow: "0px 0px 3px rgba(0, 0, 0, 0.3)",
+          gap: 5,
         }}
       >
-        {category}
+        <img src={categoryIcon} alt="categoryIcon" width={16} height={16} />
+        {displayCategory}
       </div>
     </div>
   );
