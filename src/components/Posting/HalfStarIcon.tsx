aaf19196f32b1bcf5403521b50c88162766e9fa2
H0onnn <dukei2017@naver.com>
2023-08-21 15:34:07 +0900

    Feat : 별점 SVG 컴포넌트화

---
+++ src/components/Posting/HalfStarIcon.tsx
@@ -0,0 +1,21 @@
+/** @jsxImportSource @emotion/react */
+import { starStyle } from "./StarRating";
+
+const HalfStarIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" css={starStyle}>
+      <defs>
+        <linearGradient id="halfGradient">
+          <stop offset="50%" stopColor="#FFD700" />
+          <stop offset="50%" stopColor="#e4e5e9" />
+        </linearGradient>
+      </defs>
+      <path
+        fill="url(#halfGradient)"
+        d="M12 2l2.4 7.2h7.6l-6 4.8 2.4 7.2-6-4.8-6 4.8 2.4-7.2-6-4.8h7.6z"
+      />
+    </svg>
+  );
+};
+
+export default HalfStarIcon;
