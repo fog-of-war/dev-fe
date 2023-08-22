3f1067ff682d865d1b2525449852d366349f2964
H0onnn <dukei2017@naver.com>
2023-08-22 20:45:39 +0900

    Feat : 게시글 관련 axios 로직 추가

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,29 +1,31 @@
 import { PostingData } from "../pages/posting/UploadPage";
 import { axiosBase } from "./axios";
 
-export const uploadPost = async (data: PostingData) => {
-  axiosBase.post("v1/posts", data);
+export const getMyPosts = async (): Promise<PostingData[]> => {
+  const response = await axiosBase.get<PostingData[]>("/v1/posts/me");
+  return response.data;
 };
 
-// export const axiosBase = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
+export const uploadPost = async (data: PostingData): Promise<PostingData> => {
+  const response = await axiosBase.post<PostingData>("v1/posts", data);
+  const newPosting = response.data;
 
-// axiosBase.interceptors.request.use(
-//   (config) => {
-//     const accessToken = JSON.parse(
-//       localStorage.getItem("accessToken") ?? "{}"
-//     ).access_token;
-//     if (accessToken) {
-//       // accessToken이 있는 경우 헤더에 토큰을 추가합니다.
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
+  return newPosting;
+};
+
+// export const updatePost = async (post: PostingData): Promise<PostingData> => {
+
+//   const data = {
+//   post_star_rating: post.post_star_rating,
+//   post_description: post.post_description,
 //   }
-// );
+
+//   const response = await axiosBase.patch<PostingData>(`/v1/posts/${post.id}`, data);
+//   const updatedPost = response.data;
+
+//   return updatedPost;
+// };
+
+export const deleteTodo = async (id: number): Promise<void> => {
+  await axiosBase.delete(`/v1/posts/${id}`);
+};
