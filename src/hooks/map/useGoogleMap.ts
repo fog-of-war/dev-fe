812f0f14021fe6b84acda59be68a400436c5fb7e
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-08-24 22:21:42 +0900

    Feat : 마커 클릭시에 동적으로 마커 있는 위치로 이동하게 만들기

--- src/hooks/map/useGoogleMap.ts
+++ src/hooks/map/useGoogleMap.ts
@@ -77,12 +77,20 @@ const useGoogleMap = (mapRef: MutableRefObject<google.maps.Map | null>) => {
     }, 500); // 디바운스 딜레이 (예: 500 밀리초)
   };
 
+  // 클릭한 마커의 위치로 지도 이동
+  const handleMarkerClick = (markerPosition: google.maps.LatLngLiteral) => {
+    if (mapRef.current) {
+      mapRef.current.panTo(markerPosition);
+    }
+  };
+
   return {
     view,
     handleCurrentLocationClick,
     handlePolygonClick,
     handleZoomChange,
     handleMapChange,
+    handleMarkerClick,
   };
 };
 
