130f1ce2cc326bd499e02bda4bf600b8b79eb64e
H0onnn <dukei2017@naver.com>
2023-08-26 21:20:33 +0900

    Feat : 유저 데이터 받아오는 axios 로직 추가

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,6 +1,13 @@
 import { ProfileData } from "../pages/auth/ProfileSetupPage";
+import { UserData } from "../types/types";
 import { axiosBase } from "./axios";
 
 export const setUpProfile = async (data: ProfileData) => {
   axiosBase.patch("v1/users/me", data);
+};
+
+export const getUserData = async (): Promise<UserData[]> => {
+  const response = await axiosBase.get<UserData[]>("v1/users/me");
+
+  return response.data;
 };
