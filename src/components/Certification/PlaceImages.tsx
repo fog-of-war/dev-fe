af714bacce164924ac0db45e26bc5363b86a2719
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-09-11 22:42:19 +0900

    Feat : 장소 정보에 리뷰가 없을시에 보이는 화면 추가

--- src/components/Certification/PlaceImages.tsx
+++ src/components/Certification/PlaceImages.tsx
@@ -1,11 +1,15 @@
 /** @jsxImportSource @emotion/react */
 
+import colors from "../../constants/colors";
+
 interface PlaceImagesProps {
   images: string[];
   onClick: () => void;
 }
 
 const PlaceImages = ({ images, onClick }: PlaceImagesProps) => {
+  const hasImages = images.length > 0;
+
   return (
     <div
       css={{
@@ -15,84 +19,127 @@ const PlaceImages = ({ images, onClick }: PlaceImagesProps) => {
         width: "100%",
       }}
     >
-      <div
-        css={{
-          flex: 1,
-          position: "relative",
-          paddingTop: "50%",
-          borderRadius: 10,
-          overflow: "hidden",
-        }}
-      >
-        <img
-          src={images[0]}
-          alt="숭례문"
+      {hasImages ? (
+        <div
           css={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            inset: 0,
-            objectFit: "cover",
+            flex: 1,
+            position: "relative",
+            paddingTop: "50%",
+            borderRadius: 10,
+            overflow: "hidden",
           }}
-        />
-      </div>
-      <div
-        css={{
-          flex: 1,
-          display: "grid",
-          gridTemplateRows: "1fr 1fr",
-          gridTemplateColumns: "1fr 1fr",
-          gap: 6,
-        }}
-      >
-        {images.slice(1, 5).map((image, i) => (
-          <div
-            key={i}
+        >
+          <img
+            src={images[0]}
+            alt="사진"
             css={{
-              position: "relative",
-              paddingTop: "50%",
-              borderRadius: 10,
-              overflow: "hidden",
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              inset: 0,
+              objectFit: "cover",
             }}
-          >
-            <img
-              src={image}
-              alt={image}
+          />
+        </div>
+      ) : (
+        <div
+          css={{
+            flex: 1,
+            position: "relative",
+            paddingTop: "50%",
+            borderRadius: 10,
+            overflow: "hidden",
+            background: "linear-gradient(to bottom, #c3c3c3, #fff)",
+          }}
+        >
+          <img
+            src="/images/certificationModal/explorerIcon.png"
+            alt="탐험아이콘"
+            css={{
+              position: "absolute",
+              width: "70%",
+              height: "70%",
+              inset: "15%",
+              objectFit: "contain",
+            }}
+          />
+        </div>
+      )}
+      {images.length === 0 && (
+        <h2
+          css={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: colors.primary,
+          }}
+        >
+          이 장소의 <br />
+          첫번째 탐험자가 <br />
+          되어 보세요!
+        </h2>
+      )}
+      {hasImages && (
+        <div
+          css={{
+            flex: 1,
+            display: "grid",
+            gridTemplateRows: "1fr 1fr",
+            gridTemplateColumns: "1fr 1fr",
+            gap: 6,
+          }}
+        >
+          {images.slice(1, 5).map((image, i) => (
+            <div
+              key={i}
               css={{
-                position: "absolute",
-                width: "100%",
-                height: "100%",
-                inset: 0,
-                objectFit: "cover",
+                position: "relative",
+                paddingTop: "50%",
+                borderRadius: 10,
+                overflow: "hidden",
               }}
-            />
-            {i === 3 && (
-              <div
+            >
+              <img
+                src={image}
+                alt={image}
                 css={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
                   position: "absolute",
-                  color: "white",
-                  top: "50%",
-                  left: "50%",
-                  transform: "translate(-50%,-50%)",
-                  fontSize: 15,
+                  width: "100%",
+                  height: "100%",
+                  inset: 0,
+                  objectFit: "cover",
                 }}
-                onClick={onClick}
-              >
-                <img
-                  src="/images/plusIcon.png"
-                  alt="plus_icon"
-                  width={14}
-                  height={14}
-                />
-                더보기
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
+              />
+              {i === 3 && (
+                <div
+                  css={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    position: "absolute",
+                    color: "white",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%,-50%)",
+                    fontSize: 15,
+                  }}
+                  onClick={onClick}
+                >
+                  <img
+                    src="/images/plusIcon.png"
+                    alt="plus_icon"
+                    width={14}
+                    height={14}
+                  />
+                  더보기
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
