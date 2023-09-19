136b8b652335b60c56fe72d74afc8661458dcbce
H0onnn <dukei2017@naver.com>
2023-09-19 15:10:31 +0900

    Feat : 리뷰 데이터를 관리하는 context 작성

---
+++ src/context/ReviewContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState, ReactNode } from "react";
+import { UpdatedReview } from "../types/types";
+
+interface ReviewContextProps {
+  updateReview: UpdatedReview;
+  setUpdateReview: React.Dispatch<React.SetStateAction<UpdatedReview>>;
+}
+
+const defaultReview: UpdatedReview = {
+  post_star_rating: 5,
+  post_description: "",
+};
+
+const ReviewContext = createContext<ReviewContextProps | null>(null);
+
+export const useReviewContext = (): ReviewContextProps => {
+  const context = useContext(ReviewContext);
+  if (!context) {
+    throw new Error("ReviewContextProvider에 문제가 발생했습니다.");
+  }
+  return context;
+};
+
+interface ReviewContextProviderProps {
+  children: ReactNode;
+  initialReview?: UpdatedReview;
+}
+
+export const ReviewContextProvider = ({
+  children,
+}: ReviewContextProviderProps) => {
+  const [updateReview, setUpdateReview] =
+    useState<UpdatedReview>(defaultReview);
+
+  return (
+    <ReviewContext.Provider
+      value={{
+        updateReview,
+        setUpdateReview,
+      }}
+    >
+      {children}
+    </ReviewContext.Provider>
+  );
+};
