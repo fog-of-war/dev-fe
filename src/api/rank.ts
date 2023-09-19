f45c827b8e8e2c80ddcde74a0dd267bdd99ef3fc
H0onnn <dukei2017@naver.com>
2023-09-19 19:51:37 +0900

    Feat : 전체 랭킹 불러오는 api 로직 추가

--- src/api/rank.ts
+++ src/api/rank.ts
@@ -4,3 +4,8 @@ export const getMyRank = async () => {
   const response = await axiosBase.get("v1/ranks/me");
   return response.data;
 };
+
+export const getAllRank = async () => {
+  const response = await axiosBase.get("v1/ranks");
+  return response.data;
+};
