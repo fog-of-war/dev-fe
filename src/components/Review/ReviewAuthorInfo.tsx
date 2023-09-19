9be6947609fc45b4cbfaf71e761a2773ffefb68b
H0onnn <dukei2017@naver.com>
2023-09-19 15:07:47 +0900

    Feat : 리뷰 수정, 삭제 로직 작성

--- src/components/Review/ReviewAuthorInfo.tsx
+++ src/components/Review/ReviewAuthorInfo.tsx
@@ -1,30 +1,85 @@
 /** @jsxImportSource @emotion/react */
-import { AuthorInfo } from "../../types/types";
+import { PostAuthor, PlacePost, PlaceData } from "../../types/types";
 import ReviewAuthor from "./ReviewAuthor";
 import ReviewEditButton from "../UI/ReviewEditButton";
+import { userDataState } from "../../store/userAtom";
+import { useRecoilValue } from "recoil";
+import { useReviewContext } from "../../context/ReviewContext";
+import { deletePost, updatePost } from "../../api/post";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 interface ReviewAuthorInfoProps {
-  authorInfo: AuthorInfo;
-  isEditing?: boolean;
-  setIsEditing: (isEditing: boolean) => void;
+  postId: PlacePost["post_id"];
+  placeId: PlaceData["place_id"];
+  authorInfo: PostAuthor;
+  isEditing: boolean;
+  setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
 }
-
-const loggedInUserId = "1";
 
 const ReviewAuthorInfo = ({
   authorInfo,
-  isEditing = false,
+  isEditing,
+  postId,
+  placeId,
   setIsEditing,
 }: ReviewAuthorInfoProps) => {
+  const userData = useRecoilValue(userDataState);
+  const { updateReview } = useReviewContext();
+  const navigate = useNavigate();
+
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
-  const handleDeleteClick = () => {};
+  const handleDeleteClick = async () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      try {
+        await deletePost(postId);
+        toast.success("게시글이 삭제되었습니다.", {
+          id: "delete-success",
+        });
+        navigate(`/reviewList/${placeId}`);
+      } catch (error: any) {
+        toast.error(
+          error.response?.data?.message ||
+            "게시글 삭제 중 오류가 발생했습니다.",
+          {
+            id: "delete-error",
+          }
+        );
+      }
+    }
+  };
 
-  const handleCompleteClick = () => {
+  const handleCancelClick = () => {
     setIsEditing(false);
   };
+
+  const handleCompleteClick = async () => {
+    console.log(updateReview);
+
+    try {
+      await updatePost(postId, updateReview);
+      toast.success("게시글이 수정되었습니다.", {
+        id: "update-success",
+      });
+
+      // 2초 딜레이 후 navigate 실행
+      setTimeout(() => {
+        navigate(`/reviewList/${placeId}`);
+        setIsEditing(false);
+      }, 1000);
+    } catch (error: any) {
+      toast.error(error.response.data.message, {
+        id: "update-error",
+      });
+    }
+  };
+
+  console.log("authorInfo", authorInfo);
+  console.log("userData", userData);
+
   return (
     <div
       css={{
@@ -33,7 +88,7 @@ const ReviewAuthorInfo = ({
       }}
     >
       <ReviewAuthor authorInfo={authorInfo} />
-      {authorInfo._id === loggedInUserId && (
+      {authorInfo.user_id === userData?.user_id && (
         <div
           css={{
             display: "flex",
@@ -42,9 +97,14 @@ const ReviewAuthorInfo = ({
           }}
         >
           {isEditing ? (
-            <ReviewEditButton buttonType="edit" onClick={handleCompleteClick}>
-              완료
-            </ReviewEditButton>
+            <>
+              <ReviewEditButton buttonType="edit" onClick={handleCompleteClick}>
+                완료
+              </ReviewEditButton>
+              <ReviewEditButton buttonType="delete" onClick={handleCancelClick}>
+                취소
+              </ReviewEditButton>
+            </>
           ) : (
             <>
               <ReviewEditButton
