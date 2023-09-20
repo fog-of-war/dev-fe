d4830fdd29768a8ad299b168bdb0a78bc0961c4a
H0onnn <dukei2017@naver.com>
2023-09-20 17:56:54 +0900

    Feat : ranking data api 로직 추가

--- src/api/rank.ts
+++ src/api/rank.ts
@@ -1,11 +1,17 @@
 import { axiosBase } from "./axios";
+import { UserRank, AllUserRank, RegionRank } from "../types/types";
 
-export const getMyRank = async () => {
+export const getMyRank = async (): Promise<UserRank> => {
   const response = await axiosBase.get("v1/ranks/me");
   return response.data;
 };
 
-export const getAllRank = async () => {
+export const getAllRank = async (): Promise<AllUserRank[]> => {
   const response = await axiosBase.get("v1/ranks");
+  return response.data;
+};
+
+export const getRegionRank = async (): Promise<RegionRank[]> => {
+  const response = await axiosBase.get("v1/ranks/region");
   return response.data;
 };
