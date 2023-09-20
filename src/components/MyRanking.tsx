e189055170a38f4e3101cab9fb7eab10bc6897b9
H0onnn <dukei2017@naver.com>
2023-09-20 22:29:31 +0900

    Design : css 수정

--- src/components/MyRanking.tsx
+++ src/components/MyRanking.tsx
@@ -29,6 +29,7 @@ const MyRanking = () => {
           padding: "25px",
           borderRadius: "15px",
           gap: "25px",
+          justifyContent: "center",
         }}
       >
         {regionRankData?.map((data, i) => (
