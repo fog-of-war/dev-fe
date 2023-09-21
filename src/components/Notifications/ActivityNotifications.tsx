927493f743f9041a0a032a5cc591eecbfe3745df
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-09-21 20:14:38 +0900

    Feat : 활동 알림에도 TimeAgo적용

--- src/components/Notifications/ActivityNotifications.tsx
+++ src/components/Notifications/ActivityNotifications.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import colors from "../../constants/colors";
 import B2 from "../UI/B2";
 import B3 from "../UI/B3";
+import TimeAgo from "./TimeAgo";
 
 interface Notification {
   user_nickname: string;
@@ -21,20 +22,20 @@ const ActivityNotifications = () => {
       comment_id: 1,
       user_nickname: "모두다차차차",
       comment_text: "여기 탕후루 짱맛인뎅",
-      comment_created_at: "1시간전",
+      comment_created_at: "2023-09-21T07:46:44.824Z",
     },
     {
       comment_id: 2,
       user_nickname: "다함께차차차",
       comment_text:
         "안녕하세요 글자테스트입니다. 이거는스무자가넘는문자입니다.",
-      comment_created_at: "2시간전",
+      comment_created_at: "2023-09-19T07:46:44.824Z",
     },
     {
       comment_id: 3,
       user_nickname: "갯마을차차차",
       comment_text: "여기 탕후루 짱맛인뎅",
-      comment_created_at: "3일전",
+      comment_created_at: "2023-07-19T07:46:44.824Z",
     },
   ];
 
@@ -91,7 +92,7 @@ const ActivityNotifications = () => {
                 ? `${notification.comment_text.slice(0, 20)}...`
                 : notification.comment_text}{" "}
               <span style={{ color: colors.lightGrey }}>
-                {notification.comment_created_at}
+                <TimeAgo timestamp={notification.comment_created_at} />
               </span>
             </B3>
           </div>
