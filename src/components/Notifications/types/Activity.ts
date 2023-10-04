726f51b4a8961377958bbc3e30855de355467709
yubin <shin.yubin18@gmail.com>
2023-10-04 15:59:31 +0900

    Refactor : 알림부분 타입설정

---
+++ src/components/Notifications/types/Activity.ts
@@ -0,0 +1,8 @@
+export interface Activity {
+  alerted_user_id: number;
+  comment_created_at: string;
+  comment_id: number;
+  comment_text: string;
+  user_image_url: string;
+  user_nickname: string;
+}
---
+++ src/components/Notifications/types/Notification.ts
@@ -0,0 +1,8 @@
+export interface Notification {
+  place_id: number;
+  place_name: string;
+  post_id: number;
+  post_image_url: string;
+  region_name: string;
+  post_created_at: string;
+}
