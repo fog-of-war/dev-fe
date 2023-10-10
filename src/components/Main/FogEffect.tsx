b800331cc1c2014f16e6e2b63597e145f3522f4d
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-10-10 21:48:44 +0900

    Design : 안개 이미지 위치 변경

--- src/components/Main/FogEffect.tsx
+++ src/components/Main/FogEffect.tsx
@@ -14,7 +14,7 @@ const FogEffect: React.FC<FogEffectProps> = ({ isLoaded }) => {
           style={{
             position: "absolute",
             top: 20,
-            left: 0,
+            left: -60,
             width: 300,
             height: 135,
             zIndex: 2,
@@ -30,7 +30,7 @@ const FogEffect: React.FC<FogEffectProps> = ({ isLoaded }) => {
           style={{
             position: "absolute",
             top: 150,
-            left: 10,
+            left: 60,
             width: 300,
             height: 135,
             zIndex: 2,
