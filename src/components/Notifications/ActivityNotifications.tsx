11d93050921300274be0ed0f931fe000770ca935
[SW4/2반] 김동균 <potter1185@gmail.com>
2023-10-12 16:20:22 +0900

    Feat : 웹소켓 연결과 웹소켓 연결 상위파일로 빼기

--- src/components/Notifications/ActivityNotifications.tsx
+++ src/components/Notifications/ActivityNotifications.tsx
@@ -1,44 +1,21 @@
 /** @jsxImportSource @emotion/react */
 
-import React from "react";
 import colors from "../../constants/colors";
 import B2 from "../UI/B2";
 import B3 from "../UI/B3";
 import TimeAgo from "./TimeAgo";
 
-interface Notification {
-  user_nickname: string;
-  user_image_url: string;
-  comment_id: number;
-  comment_text: string;
-  comment_created_at: string;
-}
+import { Activity } from "./types"; // 타입을 types 폴더로 분리
 
 const imageUrl = "images/dummyUserImage.png";
 
-const ActivityNotifications = () => {
-  const notifications = [
-    {
-      comment_id: 1,
-      user_nickname: "모두다차차차",
-      comment_text: "여기 탕후루 짱맛인뎅",
-      comment_created_at: "2023-09-21T07:46:44.824Z",
-    },
-    {
-      comment_id: 2,
-      user_nickname: "다함께차차차",
-      comment_text:
-        "안녕하세요 글자테스트입니다. 이거는스무자가넘는문자입니다.",
-      comment_created_at: "2023-09-19T07:46:44.824Z",
-    },
-    {
-      comment_id: 3,
-      user_nickname: "갯마을차차차",
-      comment_text: "여기 탕후루 짱맛인뎅",
-      comment_created_at: "2023-07-19T07:46:44.824Z",
-    },
-  ];
+interface ActivityNotificationsProps {
+  activities: Activity[]; // 활동 알림 데이터 배열
+}
 
+const ActivityNotifications: React.FC<ActivityNotificationsProps> = ({
+  activities,
+}) => {
   // 'x' 아이콘 클릭 시, 해당 알림 삭제
   const handleDeleteClick = (notificationId: number) => {
     console.log("지워!");
@@ -47,9 +24,9 @@ const ActivityNotifications = () => {
   return (
     <div>
       <hr css={{ width: "100%", border: `0.5px solid ${colors.paleGrey}` }} />
-      {notifications.map((notification) => (
+      {activities.map((activity) => (
         <div
-          key={notification.comment_id}
+          key={activity.comment_id}
           css={{
             display: "flex",
             alignItems: "center",
@@ -82,24 +59,24 @@ const ActivityNotifications = () => {
           <div css={{ flex: 1 }}>
             <B2 css={{ color: colors.mediumGrey, fontWeight: 600 }}>
               <span style={{ color: colors.primary }}>
-                {notification.user_nickname}
+                {activity.user_nickname}
               </span>
               님이 댓글을 남겼어요
             </B2>
             <br />
             <B3 css={{ color: colors.mediumGrey, fontWeight: 400 }}>
-              {notification.comment_text.length > 20
-                ? `${notification.comment_text.slice(0, 20)}...`
-                : notification.comment_text}{" "}
+              {activity.comment_text.length > 20
+                ? `${activity.comment_text.slice(0, 20)}...`
+                : activity.comment_text}{" "}
               <span style={{ color: colors.lightGrey }}>
-                <TimeAgo timestamp={notification.comment_created_at} />
+                <TimeAgo timestamp={activity.comment_created_at} />
               </span>
             </B3>
           </div>
           <img
             src="images/xIcon.png"
             alt="삭제 아이콘"
-            onClick={() => handleDeleteClick(notification.comment_id)}
+            onClick={() => handleDeleteClick(activity.comment_id)}
             css={{
               cursor: "pointer",
               width: 12,
