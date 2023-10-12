da1e10614b850c6029bf5a2ca7181134c92a2891
H0onnn <dukei2017@naver.com>
2023-10-12 18:19:37 +0900

    Feat: 회원 탈퇴시 캐싱된 유저 데이터 삭제

--- src/components/EditProfile/DeleteProfileButton.tsx
+++ src/components/EditProfile/DeleteProfileButton.tsx
@@ -6,18 +6,21 @@ import Button from "../UI/Button";
 import toast from "react-hot-toast";
 import { LINK } from "../../constants/links";
 import { clearAllLocalStorage } from "../../utils/localStorage";
+import useAuth from "../../hooks/useAuth";
 
 const DeleteProfileButton = () => {
   const navigate = useNavigate();
+  const { clearCurrentUser } = useAuth();
 
   const handleDeleteUser = async () => {
     if (window.confirm("정말로 회원 탈퇴 하시겠습니까?")) {
       await deleteUser();
+      clearCurrentUser();
       clearAllLocalStorage();
+      navigate(LINK.AUTH_PAGE);
       toast.success("회원 탈퇴가 완료되었습니다.", {
         id: "delete-user-success",
       });
-      navigate(LINK.AUTH_PAGE);
     }
   };
 
