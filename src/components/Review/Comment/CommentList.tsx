e2ec9ecb4916817d3ad6b06980e877dcc8d2b45b
H0onnn <dukei2017@naver.com>
2023-10-13 17:42:45 +0900

    Feat : 새로 생성된 댓글이 즉시 반영되도록 state 관리

--- src/components/Review/Comment/CommentList.tsx
+++ src/components/Review/Comment/CommentList.tsx
@@ -1,24 +1,37 @@
+import { useState } from "react";
 import styled from "@emotion/styled";
 import CommentItem from "./CommentItem";
+import CommentTextArea from "./CommentTextArea";
 import { PostComment } from "../../../types/types";
 
 interface CommentListProps {
   commentsData: PostComment[];
+  postId: number;
 }
 
-const CommentList = ({ commentsData }: CommentListProps) => {
+const CommentList = ({ commentsData, postId }: CommentListProps) => {
+  const [comments, setComments] = useState<PostComment[]>(commentsData);
+
   return (
-    <CommentsContainer>
-      {commentsData.map((comment) => (
-        <CommentItem
-          key={comment.comment_id}
-          comment_author_image_url={comment.comment_author.user_image_url}
-          comment_author_nickname={comment.comment_author.user_nickname}
-          comment_text={comment.comment_text}
-          comment_date={comment.comment_created_at}
+    <>
+      <CommentsContainer>
+        {comments.map((comment) => (
+          <CommentItem
+            key={comment.comment_id}
+            comment_author_image_url={comment.comment_author.user_image_url}
+            comment_author_nickname={comment.comment_author.user_nickname}
+            comment_text={comment.comment_text}
+            comment_date={comment.comment_created_at}
+          />
+        ))}
+        <CommentTextArea
+          postId={postId}
+          onNewComment={(newComment) =>
+            setComments((prevComments) => [...prevComments, newComment])
+          }
         />
-      ))}
-    </CommentsContainer>
+      </CommentsContainer>
+    </>
   );
 };
 
@@ -31,7 +44,4 @@ const CommentsContainer = styled.div`
   position: relative;
   top: 35px;
   width: 100%;
-  height: 100%;
-  max-height: 300px;
-  overflow-y: auto;
 `;
