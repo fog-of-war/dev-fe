571f80e5656b0b78bed86e5ecfacbe9def066115
jang_yoonsu <shagrat8546@gmail.com>
2023-10-23 18:13:47 +0900

    feat : 검색 결과 없음 표시 추가

---
+++ src/components/Explore/Search/NoSearchData.tsx
@@ -0,0 +1,19 @@
+/** @jsxImportSource @emotion/react */
+
+const NoSearchData = () => {
+  return (
+    <div
+      css={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      검색결과가 없습니다.
+    </div>
+  );
+};
+
+export default NoSearchData;
