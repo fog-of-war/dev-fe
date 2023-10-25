a45e274efa6dd1c88dba7f079ffff8cfaf7ed4c6
H0onnn <dukei2017@naver.com>
2023-10-25 13:38:29 +0900

    Feat: 공통 페이지 헤더 컴포넌트 작성

---
+++ src/components/UI/PageHeader.tsx
@@ -0,0 +1,68 @@
+/** @jsxImportSource @emotion/react */
+import { useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+import BackButton from "./BackButton";
+
+interface PageHeaderProps {
+  headerTitle: string;
+  reviewCount?: number;
+}
+
+const PageHeader = ({ headerTitle, reviewCount }: PageHeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <PageHeaderLayout>
+      <BackButtonWrapper>
+        <BackButton onClick={handleBackButtonClick} />
+      </BackButtonWrapper>
+      <HeaderTitleContainer>
+        <HeaderTitle>{headerTitle}</HeaderTitle>
+        {reviewCount && <ReviewCount>({reviewCount})</ReviewCount>}
+      </HeaderTitleContainer>
+      <EmptyDiv />
+    </PageHeaderLayout>
+  );
+};
+
+export default PageHeader;
+
+const PageHeaderLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 420px;
+  height: 50px;
+  position: fixed;
+  top: 0;
+  background-color: #fff;
+`;
+
+const BackButtonWrapper = styled.div`
+  width: 10px;
+  height: 20px;
+  padding-left: 30px;
+`;
+
+const HeaderTitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+  align-items: center;
+`;
+
+const HeaderTitle = styled.h1`
+  font-size: 17px;
+  font-weight: 400;
+`;
+
+const ReviewCount = styled.p``;
+
+const EmptyDiv = styled.div`
+  padding-right: 30px;
+`;
