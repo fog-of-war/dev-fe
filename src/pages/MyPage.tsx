7e7b04eb489f376d06ff49b86975fe6ba9650b32
H0onnn <dukei2017@naver.com>
2023-10-25 14:24:12 +0900

    Feat: 공용 버튼 컴포넌트 적용 및 로그아웃 버튼 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -3,12 +3,51 @@ import styled from "@emotion/styled";
 import UserProfile from "../components/ProfileCard/UserProfile";
 import AdvPlaceList from "../components/AdventurePlace/AdvPlaceList";
 import PageHeader from "../components/UI/PageHeader";
+import ProfileActionButton from "../components/EditProfile/ProfileActionButton";
+import { useNavigate } from "react-router-dom";
+import { deleteUser } from "../api/user";
+import { logout } from "../api/auth";
+import { LINK } from "../constants/links";
+import { clearAllLocalStorage } from "../utils/localStorage";
+import useAuth from "../hooks/useAuth";
 
 const MyPage = () => {
+  const navigate = useNavigate();
+  const { clearCurrentUser } = useAuth();
+
+  const handleLogout = async () => {
+    clearCurrentUser();
+    clearAllLocalStorage();
+    navigate(LINK.AUTH_PAGE);
+  };
+
+  const handleDelete = async () => {
+    await deleteUser();
+    clearCurrentUser();
+    clearAllLocalStorage();
+    navigate(LINK.AUTH_PAGE);
+  };
+
   return (
     <>
       <PageHeader headerTitle="마이페이지" />
       <MyPageLayout>
+        <ActionButtonContainer>
+          <ProfileActionButton
+            buttonType="logout"
+            onConfirm={handleLogout}
+            confirmMessage="로그아웃 하시겠습니까?"
+            successMessage="로그아웃이 완료되었습니다."
+            buttonText="로그아웃"
+          />
+          <ProfileActionButton
+            buttonType="delete"
+            onConfirm={handleDelete}
+            confirmMessage="회원 탈퇴시 프로필 데이터가 모두 삭제됩니다."
+            successMessage="회원 탈퇴가 완료되었습니다."
+            buttonText="회원 탈퇴"
+          />
+        </ActionButtonContainer>
         <UserProfile />
         <AdvPlaceList />
       </MyPageLayout>
@@ -27,4 +66,12 @@ const MyPageLayout = styled.div`
   height: 100%;
   padding: 20px;
   padding-top: 60px;
+`;
+
+const ActionButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: row;
+  gap: 10px;
 `;
