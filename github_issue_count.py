02e0d9371dd2de4249dc213913620006720ccf17
yubin <shin.yubin18@gmail.com>
2023-10-31 20:15:13 +0900

    Workflow : 이 오류는 Discord.py의 버전 2 이상에서 발생하는 문제로, 이전 버전과는 다른 방식으로 봇을 초기화해야 합니다. Discord.py 버전 2부터는 async def main()을 사용하여 비동기적인 메인 함수를 생성하고, 이 함수를 asyncio.run(main())으로 실행해야 합니다.

--- github_issue_count.py
+++ github_issue_count.py
@@ -1,17 +1,18 @@
 import os
 import requests
 import discord
-import asyncio  # asyncio 모듈을 가져옵니다.
+import asyncio
 
 # GitHub API 토큰 및 디스코드 봇 토큰 설정
 GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 DISCORD_CHANNEL_ID = os.environ["DISCORD_CHANNEL_ID"]
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 
 async def send_github_issue_count():
-    await client.wait_until_ready()  # 봇이 완전히 준비될 때까지 기다립니다.
+    await client.wait_until_ready()
 
     while not client.is_closed():
         # GitHub API를 사용하여 이슈 가져오기
@@ -30,16 +31,18 @@ async def send_github_issue_count():
                     assignee_count[assignee] = 1
 
             # 디스코드로 보내기
-            channel = client.get_channel(int(DISCORD_CHANNEL_ID))  # 디스코드 채널 ID를 정수로 변환
+            channel = client.get_channel(int(DISCORD_CHANNEL_ID))
             for assignee, count in assignee_count.items():
                 await channel.send(f"{assignee}: {count}개의 이슈")
 
-        await asyncio.sleep(60)  # 24시간(하루) 대기 후에 다시 실행
+        await asyncio.sleep(60)  # 60초(1분) 대기 후에 다시 실행
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
 
-# 봇 실행
-client.loop.create_task(send_github_issue_count())
-client.run(DISCORD_BOT_TOKEN)
+async def main():
+    await client.start(DISCORD_BOT_TOKEN)
+
+if __name__ == "__main__":
+    asyncio.run(main())
