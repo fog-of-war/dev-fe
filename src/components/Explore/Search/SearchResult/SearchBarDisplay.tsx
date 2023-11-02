c5e7f2e456e0fe282f923a99e82562a1e3e9b899
jang_yoonsu <shagrat8546@gmail.com>
2023-11-02 14:41:19 +0900

    refactor : 서치 페이지 리팩토링 - Explore 스타일 파일 Search 스타일 파일로 병합 - 각 페이지 맥락에 맞게 스타일드 컴포넌트 이름 변경해서 사용 - 실시간 검색결과 패널에서 deferredValue, debouncingInput 상위 컴포넌트로 위임

--- src/components/Explore/Search/SearchResult/SearchBarDisplay.tsx
+++ src/components/Explore/Search/SearchResult/SearchBarDisplay.tsx
@@ -2,7 +2,7 @@
 
 import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
-import { SearchBarLayout } from "../../Explore.styles";
+import { SearchBarLayout } from "../styles/Explore.styles";
 
 interface SearchBarDisplayProps {
   value?: string;
--- src/components/Explore/Search/styles/Search.styles.ts
+++ src/components/Explore/Search/styles/Search.styles.ts
@@ -1,6 +1,29 @@
 import styled from "@emotion/styled";
 import colors from "../../../../constants/colors";
 
+export const SearchPageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  gap: 12px;
+  background-color: white;
+`;
+
+export const SearchBarLayout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 20px;
+  width: 100%;
+  height: 45px;
+  padding: 0 20px;
+  border-radius: 10px;
+  background: white;
+  z-index: 70;
+`;
+
 export const SearchList = styled.ul`
   width: calc(100% + 40px);
   margin-left: -20px;
