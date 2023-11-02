509fe8d7ed9124b0c9c96f8799c949caa5656a67
H0onnn <dukei2017@naver.com>
2023-11-02 20:50:26 +0900

    Refactor: 수정 상태의 별 아이콘 크기 수정

--- src/components/Posting/EmptyStarIcon.tsx
+++ src/components/Posting/EmptyStarIcon.tsx
@@ -1,13 +1,17 @@
 /** @jsxImportSource @emotion/react */
 import { starStyle } from "./StarRating";
 
-const EmptyStarIcon = () => {
+interface StarIconProps {
+  isEditing: boolean;
+}
+
+const EmptyStarIcon = ({ isEditing = false }: StarIconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="#e4e5e9"
       viewBox="0 0 24 24"
-      css={starStyle}
+      css={starStyle(isEditing)}
     >
       <path d="M12 2l2.4 7.2h7.6l-6 4.8 2.4 7.2-6-4.8-6 4.8 2.4-7.2-6-4.8h7.6z" />
     </svg>
