be6a49ee496131951fd1212ea5bb6b5e58ceaa14
Yubin Shin <68121478+YubinShin@users.noreply.github.com>
2023-11-25 20:55:32 +0900

    Fix : github_issue_count.py  의존성 추가 및 이슈 없을 시 메세지 x

--- github_issue_count.py
+++ github_issue_count.py
@@ -1,14 +1,27 @@
+import os
+import requests
+import discord
+from discord.ext import commands, tasks
+import time
+from datetime import datetime
+
+# GitHub API 토큰 및 디스코드 봇 토큰 설정
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
+DISCORD_CHANNEL_ID = os.environ["DISCORD_CHANNEL_ID"]
+
+client = discord.Client(intents=discord.Intents.default())
+
 async def send_github_issue_count_once():
     # GitHub API를 사용하여 이슈 가져오기
     headers = {
         "Authorization": f"token {GITHUB_TOKEN}"
     }
     response = requests.get("https://api.github.com/repos/fog-of-war/dev-fe/issues", headers=headers)
-       
     if response.status_code == 200:
         issues = response.json()
-        
-        # 이슈가 없으면 메시지를 보내지 않고 함수 종료
+
+        # 이슈가 없는 경우 메시지를 보내지 않고 함수를 종료합니다.
         if not issues:
             return
 
@@ -23,19 +36,35 @@ async def send_github_issue_count_once():
                 assignee_count[assignee] = 1
 
         # 디스코드로 보내기
-        channel = client.get_channel(int(DISCORD_CHANNEL_ID))  # 디스코드 채널 ID를 정수로 변환
+        channel = client.get_channel(int(DISCORD_CHANNEL_ID))
 
         # 메시지 생성
         message = f"## 📅 **{current_date}**\n "
         for assignee, count in assignee_count.items():
             message += f"💡 **{assignee}**: {count}개의 이슈가 남아있습니다.\n"
+            # Collect issue titles within a single code block
             code_block = "```md\n"
             for issue in issues:
                 if issue["assignee"] and issue["assignee"]["login"] == assignee:
                     issue_title = issue["title"]
                     code_block += f"{issue_title}\n"
             code_block += "```"
+            
             message += code_block
             message += "\n"
 
         await channel.send(message)
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+    
+    # 봇 실행 후 한 번만 작업 실행
+    await send_github_issue_count_once()
+    
+    # 작업이 완료되면 봇 종료
+    await client.close()
+
+# 봇 실행
+client.run(DISCORD_BOT_TOKEN)
